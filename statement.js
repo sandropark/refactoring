3842331d823fc282a6f6710e884d59dadd3650d5
sandropark <ticoba@naver.com>
2025-03-29 15:09:07 +0900

    html 렌더함수 추가

--- statement.js
+++ statement.js
@@ -21,4 +21,17 @@ export default function statement(invoice, plays) {
     result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
     return result;
   }
+
+  function renderHtml(data) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += `<table>\n`;
+    result += `<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n`;
+    for (let perf of data.performances) {
+      result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td><td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += `</table>\n`;
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}점</em></p>\n`;
+    return result;
+  }
 }
