875a433190893de73031b49dc93f3e334d0c1f63
sandropark <ticoba@naver.com>
2025-03-29 13:30:38 +0900

    인자 순서 변경 및 인자명 변경

--- statement.js
+++ statement.js
@@ -10,7 +10,7 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = amountFor(play, perf);
+    let thisAmount = amountFor(perf, play);
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -29,22 +29,22 @@ function statement(invoice, plays) {
   return result;
 
   // 중첩 함수
-  function amountFor(play, perf) {
+  function amountFor(aPerformance, play) {
     let result = 0;
 
     switch (play.type) {
       case "tragedy": // 비극
         result = 40000;
-        if (perf.audience > 30) {
-          result += 1000 * (perf.audience - 30);
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
         }
         break;
       case "comedy": // 희극
         result = 30000;
-        if (perf.audience > 20) {
-          result += 10000 + 500 * (perf.audience - 20);
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
         }
-        result += 300 * perf.audience;
+        result += 300 * aPerformance.audience;
         break;
       default:
         throw new Error(`알 수 없는 장르: ${play.type}`);
